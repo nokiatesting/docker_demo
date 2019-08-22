ec4447ea200c7833824b5772b46a1491ba78b7fa
nokiatesting <33539816+nokiatesting@users.noreply.github.com>
2019-08-22 16:03:13 +0800

    Update docker_demo.py
    change test.db path

--- docker_demo.py
+++ docker_demo.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/testdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Remove warning
 db = SQLAlchemy(app)
